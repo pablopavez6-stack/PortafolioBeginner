66a478c87a03078beeca9ffc1e9ba955e34a3da9
Pablete1312 <pablopavez6@gmail.com>
2025-11-19 00:44:56 -0300

    Primer commit del proyecto benigner

--- calculadora.py
+++ calculadora.py
@@ -1,36 +1,45 @@
 
 def mostrar_menu():
-    print(f'''\nOperaciones:
-    1- Sumar
-    2- Restar
-    3- Multiplicacion
-    4- Division
-    5- Potencia
-    6- Salir''')
-    return int(input('Selecciona la operación a realizar: '))
+    while True:
+        try:
+            print(f'Calculadora'.center(50, '-'),)
+            print('''\tOperaciones
+        1- Sumar
+        2- Restar
+        3- Multiplicacion
+        4- Division
+        5- Potencia
+        6- Salir''')
+            return int(input('Selecciona la operación a realizar: '))
+        except ValueError:
+            print('Por favor ingresa la opción correcta (números)\n')
 
 def valores():
-    a = float(input('Primer número de la operación: '))
-    b = float(input('Segundo número de la operación: '))
-    return a, b
+    while True:
+        try:
+            a = float(input('Primer número de la operación: '))
+            b = float(input('Segundo número de la operación: '))
+            return a, b
+        except ValueError:
+            print('Por favor ingresa solo números.')
 
 def operacion(opcion, salir):
     if 1 <= opcion <= 5:
         a, b = valores()
 
-    if opcion == 1:
-        print(f'\tEl resultado de la suma de {a} + {b} es = {a + b}')
-    elif opcion == 2:
-        print(f'\tEl resultado de la resta de {a} - {b} es = {a - b}')
-    elif opcion == 3:
-        print(f'\tEl resultado de la multiplicación de {a} * {b} es = {a * b}')
-    elif opcion == 4:
-        if b == 0:
-            print(f'No se puede dividir por 0')
-        else:
-            print(f'\tEl resultado de la division de {a} / {b} es = {a / b}')
-    elif opcion == 5:
-        print(f'\tEl resultado de la potencia de {a} ^ {b} es = {a ** b}')
+        if opcion == 1:
+            print(f'\tEl resultado de la suma de {a} + {b} es = {a + b}')
+        elif opcion == 2:
+            print(f'\tEl resultado de la resta de {a} - {b} es = {a - b}')
+        elif opcion == 3:
+            print(f'\tEl resultado de la multiplicación de {a} * {b} es = {a * b}')
+        elif opcion == 4:
+            if b == 0:
+                print(f'No se puede dividir por 0')
+            else:
+                print(f'\tEl resultado de la division de {a} / {b} es = {a / b}')
+        elif opcion == 5:
+            print(f'\tEl resultado de la potencia de {a} ^ {b} es = {a ** b}')
     elif opcion == 6:
         print('Saliendo...')
         salir = True
